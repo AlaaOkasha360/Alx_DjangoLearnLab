882b52ee23bfb279672c5bf0ea92fda540a25a24
Alaa Okasha <mxmmotnt1@gmail.com>
2025-09-07 20:44:10 +0300

    Fixed smaples in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,13 @@
 from .models import Author, Book, Library, Librarian
 
-books = Book.objects.filter(author__name = "Alaa")
 
-lib_books = Library.objects.get(name="House").books.all()
+def query_all_book_by_author(author_name):
+    books = Book.objects.filter(author__name=author_name)
 
-librarian = Librarian.objects.get(library__name = "House")
+
+def list_all_books_in_library(library_name):
+    lib_books = Library.objects.get(name=library_name)
+
+
+def retrieve_librarian_for_library(library_name):
+    librarian = Librarian.objects.get(library__name=library_name)
