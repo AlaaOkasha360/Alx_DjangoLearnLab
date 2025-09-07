3bf947d69b2846c4c465439c27e0f86547ebd065
Alaa Okasha <mxmmotnt1@gmail.com>
2025-09-07 22:42:32 +0300

    renamed some classes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,7 +20,7 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         library = self.get_object()
 
-class RegisterView(CreateView):
+class register(CreateView):
     form_class = UserCreationForm
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
