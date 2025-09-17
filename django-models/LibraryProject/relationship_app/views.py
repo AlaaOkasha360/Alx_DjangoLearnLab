d8464196f2f0ae9003e335db37f6ae958ae1f9d5
Alaa Okasha <mxmmotnt1@gmail.com>
2025-09-18 01:55:13 +0300

    Fixed some issues

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,7 +22,7 @@ class LibraryDetailView(DetailView):
         library = self.get_object()
 
 class register(CreateView):
-    UserCreationForm()
+    form_class = UserCreationForm
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
 
