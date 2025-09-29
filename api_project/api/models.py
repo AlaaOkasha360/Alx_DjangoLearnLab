eff73c16cf2bf4639e5e49ca2e5559290b87789e
Alaa Okasha <mxmmotnt1@gmail.com>
2025-09-30 00:24:34 +0300

    Implemented CRUD Operations in Book model

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
+    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=25)
