90a0a27575c9057a3500f01c6c6b4999a7abd4d6
Alaa Okasha <mxmmotnt1@gmail.com>
2025-09-30 20:29:49 +0300

    Creating Blog Post Management Features

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,10 +1,36 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ["username", "email", "password1", "password2"]
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "content"]
+        widgets = {
+            "title": forms.TextInput(
+                attrs={"placeholder": "Post title", "class": "form-control"}
+            ),
+            "content": forms.Textarea(
+                attrs={
+                    "placeholder": "Write your post...",
+                    "class": "form-control",
+                    "rows": 8,
+                }
+            ),
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title", "").strip()
+        if not title:
+            raise forms.ValidationError("Title cannot be empty.")
+        return title
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,18 +1,65 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm, UserRegisterForm
+
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Your account has been created! You can now log in.')
-            return redirect('login')
+            messages.success(
+                request, "Your account has been created! You can now log in."
+            )
+            return redirect("login")
     else:
         form = UserRegisterForm()
-    return render(request, 'blog/register.html', {'form': form})
+    return render(request, "blog/register.html", {"form": form})
+
 
 def profile(request):
-    return render(request, 'blog/profile.html')
+    return render(request, "blog/profile.html")
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    paginate_by = 10
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post-list")
