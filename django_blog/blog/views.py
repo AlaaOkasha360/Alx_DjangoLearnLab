a1ca6ae71ab64c331a98b489360839ee59e719b9
Alaa Okasha <mxmmotnt1@gmail.com>
2025-09-30 20:33:11 +0300

    Imported decorator

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -9,6 +9,7 @@ from django.views.generic import (
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm, UserRegisterForm
 
