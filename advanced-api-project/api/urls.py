ca8d9994f8a90d9591f413161adfdee2264158b1
Alaa Okasha <mxmmotnt1@gmail.com>
2025-09-30 17:42:54 +0300

    Fixed some issues

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("books/", views.BookListAPIView.as_view()),
-    path("books/<int:pk>/", views.BookDetailAPIView.as_view()),
-    path("books/create/", views.BookCreateAPIView.as_view()),
-    path("books/<int:pk>/update/", views.BookUpdateAPIView.as_view()),
-    path('"books/<int:pk>/delete/', views.BookDeleteAPIView.as_view()),
+    path("books/", views.ListView.as_view()),
+    path("books/<int:pk>/", views.DetailView.as_view()),
+    path("books/create/", views.CreateView.as_view()),
+    path("books/update/", views.UpdateView.as_view()),
+    path('"books/delete/', views.DeleteView.as_view()),
 ]
